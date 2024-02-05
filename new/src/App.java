0342b8ff37c489ce911633a9141f9234de29940a
Jun Yoshikawa <jun.yoshikawa@rakus-partners.co.jp>
2024-02-05 13:21:33 +0900

    hatena

--- new/src/App.java
+++ new/src/App.java
@@ -1,9 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
         System.out.println("こんにちは!");
-=======
-        System.out.println("Hello, ca!");
->>>>>>> develop
     }
 }
